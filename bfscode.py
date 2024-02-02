4156383c36f95327ba312f4097e9abc7adf30468
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2024-02-02 09:38:57 +0530

    update

--- bfscode.py
+++ bfscode.py
@@ -1,41 +1,43 @@
 from collections import defaultdict
-
+import random 
 class Graph:
     def __init__(self):
         self.graph = defaultdict(list)
-
+ 
     def add_edge(self, u, v):
         self.graph[u].append(v)
+    def generate_random_graph(self, num_nodes, num_edges):
+        for _ in range(num_edges):
+            u = random.randint(0, num_nodes - 1)
+            v = random.randint(0, num_nodes - 1)
+            self.add_edge(u, v)
 
+ 
     def BFS(self, start):
         visited = {node: False for node in self.graph}
-        color = {node: "white" for node in self.graph}
+        color = {node: "WHITE" for node in self.graph}
         queue = []
-
+        print("\nBefore Bfs: ",color)
         queue.append(start)
         visited[start] = True
-
+ 
         while queue:
+            print()
             current = queue.pop(0)
-            print(current, end=" ")
-
+ 
             for neighbor in self.graph[current]:
                 if not visited[neighbor]:
                     visited[neighbor] = True
                     queue.append(neighbor)
-                    color[neighbor] = "grey"
-            color[current] = "black"
+                    color[neighbor] = "GRAY"
+            color[current] = "BLACK"
+            print(f" Itration for node {current}: ",color)
+ 
+ 
 
-        print("\nColors:", color)
-
-# Example usage:
 graph = Graph()
-graph.add_edge(0, 1)
-graph.add_edge(0, 2)
-graph.add_edge(1, 2)
-graph.add_edge(2, 0)
-graph.add_edge(2, 3)
-graph.add_edge(3, 3)
 
+graph.generate_random_graph(10, 50)
+ 
 print("BFS Traversal starting from vertex 2:")
 graph.BFS(2)
